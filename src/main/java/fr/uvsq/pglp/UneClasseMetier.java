9d28719b052375eee108b4b76db53d16ef4b0cd0
uvsq21920982 <yassinemamadou97@gmail.com>
2020-03-28 15:45:00 +0100

    MAJ CLASSE METIER

--- src/main/java/fr/uvsq/pglp/UneClasseMetier.java
+++ src/main/java/fr/uvsq/pglp/UneClasseMetier.java
@@ -3,8 +3,8 @@ import java.time.LocalDateTime;
 
 public class UneClasseMetier {
     public void uneMethodeMetier(Logger logger){
-        logger.log(LocalDateTime.now()+":Début de methode !!!");
-        logger.log(LocalDateTime.now()+": Fin de methode !!");
+        logger.log(LocalDateTime.now()+":Début de methode !");
+        logger.log(LocalDateTime.now()+": Fin de methode !");
     }
 
 
